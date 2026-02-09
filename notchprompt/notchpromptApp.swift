91b0f07e43c60724f07e4d0ed4b8280351a00429
saif <saifahmedraafat@gmail.com>
2026-02-08 21:03:23 -0700

    Initial Commit

---
+++ notchprompt/notchpromptApp.swift
@@ -0,0 +1,17 @@
+//
+//  notchpromptApp.swift
+//  notchprompt
+//
+//  Created by Saif on 2026-02-08.
+//
+
+import SwiftUI
+
+@main
+struct notchpromptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
